573d0051985cb14c64b2528576fe4fa0587e802a
Eran <eran.hasarali.2001@gmail.com>
2025-04-12 15:07:49 +0530

    add separate js file

---
+++ script.js
@@ -0,0 +1,15 @@
+const myList = [
+  {
+    name: "Item 1",
+    color: "red",
+  },
+  {
+    name: "Item 2",
+    color: "blue",
+  },
+  {
+    name: "Item 3",
+    color: "green",
+  },
+];
+console.log(myList[0]?.name);
