b29f872a07eda82555e383b53775b66dffa3047e
Eran <eran.hasarali.2001@gmail.com>
2025-04-12 17:30:37 +0530

    add some codes

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const myList = {
   city: "New York",
   courses: { one: "Math", two: "Science" },
 };
-// Accessing properties using dot notation
 
 console.log(myList?.courses?.one);
+
+if (myList?.courses?.one) {
+  console.log("yes");
+}
+const myList2 = null;
+console.log(myList2?.courses?.one); // undefined
