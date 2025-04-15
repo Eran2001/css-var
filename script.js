66d42d1611b808658430072b8c4851347ef7bcb0
Eran <eran.hasarali.2001@gmail.com>
2025-04-15 20:23:14 +0530

    change name

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 const myList = {
-  name: "John",
+  name: "Eran",
   age: 30,
   city: "New York",
   courses: { one: "Math", two: "Science" },
 };
 
 console.log(myList?.courses?.one);
-
-if (myList?.courses?.one) {
-  console.log("yes");
-}
-const myList2 = null;
-console.log(myList2?.courses?.one); // undefined
